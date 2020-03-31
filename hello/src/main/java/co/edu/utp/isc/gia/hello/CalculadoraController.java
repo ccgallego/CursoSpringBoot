50e941e83c6f2a6d28ecce369545f13869e41afc
Cristian Camilo Gallego <c.gallego@utp.edu.co>
2020-03-30 20:27:46 -0500

    Agregar operaciones en el CalculadoraController

---
+++ hello/src/main/java/co/edu/utp/isc/gia/hello/CalculadoraController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.utp.isc.gia.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author utp
+ */
+@RestController
+@RequestMapping("calc")
+public class CalculadoraController {
+
+    //localhost:8080/calc/suma?num1=5.5&num2=5.5
+    @GetMapping("/suma")
+    public Double suma(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        return num1 + num2;
+    }
+
+    
+    
+    @GetMapping("/resta")
+    public Double resta(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        return num1 - num2;
+    }
+       
+    
+    @GetMapping("/multiplicacion")
+    public Double multiplicacion(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        return num1 * num2;
+    }
+
+    @GetMapping("/division")
+    public Double division(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        if (num2 == 0.0) {
+            System.out.println("No se puede dividir por cero");
+            return 0.0;
+        } else {
+            return num1 / num2;
+        }
+    }
+    
+    
+
+}
