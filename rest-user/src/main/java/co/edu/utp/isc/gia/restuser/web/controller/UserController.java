1e397b96058efef1c39e016f0b0e94a87d5639e6
Cristian Camilo Gallego <c.gallego@utp.edu.co>
2020-04-01 15:52:13 -0500

    Códigos de mensajes de éxito o error

--- rest-user/src/main/java/co/edu/utp/isc/gia/restuser/web/controller/UserController.java
+++ rest-user/src/main/java/co/edu/utp/isc/gia/restuser/web/controller/UserController.java
@@ -9,6 +9,8 @@ import co.edu.utp.isc.gia.restuser.service.impl.UserServiceImpl;
 import co.edu.utp.isc.gia.restuser.web.dto.UserDTO;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -25,36 +28,56 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("user")
 public class UserController {
-    
+
     private UserServiceImpl userService;
 
     public UserController(UserServiceImpl userService) {
         this.userService = userService;
     }
-    
+
     @PostMapping()
-    public UserDTO insert(@RequestBody UserDTO user){
-        return userService.save(user);
+    public ResponseEntity<UserDTO> insert(@RequestBody UserDTO user) {
+        UserDTO u = userService.save(user);
+        if (u != null) {
+            return ResponseEntity.ok(u);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
-    
+
     @GetMapping()
-    public List<UserDTO> getAll(){
-        return userService.findAll();
+    public ResponseEntity<List<UserDTO>> getAll() {
+        List<UserDTO> list = userService.findAll();
+        if(!list.isEmpty()){
+            return ResponseEntity.ok(list);
+        }
+        return ResponseEntity.noContent().build();
     }
-    
+
     @GetMapping("/{id}")
-    public UserDTO getOne(@PathVariable("id") Long id){
-        return userService.findOne(id);
+    public ResponseEntity<UserDTO> getOne(@PathVariable("id") Long id) {
+        UserDTO user = userService.findOne(id);
+        if(user != null){
+            return ResponseEntity.ok(user);
+        }
+        return ResponseEntity.notFound().build();
     }
-    
+
     @DeleteMapping("/{id}")
-    public UserDTO deleteUser(@PathVariable("id") Long id){
-        return userService.delete(id);
+    public ResponseEntity<UserDTO> deleteUser(@PathVariable("id") Long id) {
+        UserDTO user = userService.delete(id);
+        if(user != null){
+            return ResponseEntity.ok(user);
+        }
+        return ResponseEntity.noContent().build();
     }
-    
+
     @PutMapping("/{id}")
-    public UserDTO updateUser(@PathVariable("id") Long id, @RequestBody UserDTO user){
-      return userService.update(id, user);
+    public ResponseEntity<UserDTO> updateUser(@PathVariable("id") Long id, @RequestBody UserDTO user) {
+        UserDTO u = userService.update(id, user);
+        if (u != null) {
+            return ResponseEntity.ok(u);
+        } 
+        return ResponseEntity.badRequest().build();
     }
-    
 }
