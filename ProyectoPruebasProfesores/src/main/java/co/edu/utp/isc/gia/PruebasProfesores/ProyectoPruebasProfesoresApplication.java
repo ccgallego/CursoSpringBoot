c680060937b09080444733a50522026b5650d210
Cristian Camilo Gallego <c.gallego@utp.edu.co>
2020-04-05 21:19:30 -0500

    Estructura inicial del proyecto final

---
+++ ProyectoPruebasProfesores/src/main/java/co/edu/utp/isc/gia/PruebasProfesores/ProyectoPruebasProfesoresApplication.java
@@ -0,0 +1,13 @@
+package co.edu.utp.isc.gia.PruebasProfesores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPruebasProfesoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPruebasProfesoresApplication.class, args);
+	}
+
+}
