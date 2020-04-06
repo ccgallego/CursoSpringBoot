32dc2666f52b8ae24c60a85472c1f2b37c14cc75
Cristian Camilo Gallego <c.gallego@utp.edu.co>
2020-04-06 16:27:51 -0500

    Renombrando proyecto

---
+++ ServicioDeEvaluacion/src/test/java/co/edu/utp/isc/gia/PruebasProfesores/ProyectoPruebasProfesoresApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.isc.gia.PruebasProfesores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPruebasProfesoresApplicationTests {
+
+	/*@Test
+	void contextLoads() {
+	}*/
+
+}
